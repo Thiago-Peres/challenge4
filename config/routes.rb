898dac14f1fa87b41c15b8c2fa62d32d135449a9
Thiago Magnani Peres <thiago-peres@outlook.com>
2021-09-14 23:36:11 -0300

    add controllers - views - validacoes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "static_pages#index"
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
---
+++ db/migrate/20210914234611_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
