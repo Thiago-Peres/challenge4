1a1a279e9f383f565b3a98c3b76b1ef8ebc06a89
Thiago Magnani Peres <thiago-peres@outlook.com>
2021-09-15 00:21:13 -0300

    Add validacao de user / erro no dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    protect_from_forgery with: :exception
+
+  include SessionsHelper
+
+  private
+    def require_logged_in_user
+      unless user_signed_in?
+        flash[:danger] = 'Área restrita. Por favor, realize o login.'
+        redirect_to entrar_url
+      end
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
 
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 end
